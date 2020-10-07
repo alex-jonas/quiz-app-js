135556aa8c0844a7ca54950f576a50246ea4e9ab
Alexander Jonas <alex.jonas@gmail.com>
2020-10-07 16:15:12 +0200

    Add js navigation links

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,37 @@
-console.log('Hello Alex')
+const homeSite = document.querySelector('[data-js=home-site]')
+const bookmarksSite = document.querySelector('[data-js=bookmark-site]')
+const createSite = document.querySelector('[data-js=create-site]')
+const profileSite = document.querySelector('[data-js=profile-site]')
+
+const linkHome = document.querySelector('[data-js=link-home]')
+const linkBookmarks = document.querySelector('[data-js=link-bookmarks]')
+const linkCreate = document.querySelector('[data-js=link-create]')
+const linkProfile = document.querySelector('[data-js=link-profile]')
+
+linkHome.addEventListener('click', () => {
+  homeSite.classList.remove('d-none')
+  createSite.classList.add('d-none')
+  profileSite.classList.add('d-none')
+  bookmarksSite.classList.add('d-none')
+})
+
+linkBookmarks.addEventListener('click', () => {
+  homeSite.classList.add('d-none')
+  createSite.classList.add('d-none')
+  profileSite.classList.add('d-none')
+  bookmarksSite.classList.remove('d-none')
+})
+
+linkCreate.addEventListener('click', () => {
+  homeSite.classList.add('d-none')
+  createSite.classList.remove('d-none')
+  profileSite.classList.add('d-none')
+  bookmarksSite.classList.add('d-none')
+})
+
+linkProfile.addEventListener('click', () => {
+  homeSite.classList.add('d-none')
+  createSite.classList.add('d-none')
+  profileSite.classList.remove('d-none')
+  bookmarksSite.classList.add('d-none')
+})
